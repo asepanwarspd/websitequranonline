d992466bfe0ed09c6121ee38c65f8c02b00b4861
asepanwarspd <asep.anwar.spd@gmail.com>
2022-02-16 10:54:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ js/pages/index.js
@@ -0,0 +1,141 @@
+import apiEndpoint from '../constant/api-endpoint.js'
+
+document.addEventListener("DOMContentLoaded", main)
+
+/**
+* Main function
+*/
+async function main() {
+    displayBaseLayout()
+    const endpoint = apiEndpoint.listSurah.url
+    const loadingAnimation = getLoadingAnimationContainer()
+    const errorAnimation = getErrorAnimationContainer()
+    const mainApp = getMainAppContainer()
+
+    try {
+        mainApp.style.display = 'none'
+        loadingAnimation.style.display = 'flex'
+        errorAnimation.style.display = 'none'
+        
+        const response = await fetch(endpoint)
+        const responseJSON = await response.json()
+        const quranData = responseJSON.data
+        renderListSurahElements(quranData)
+        
+        mainApp.style.display = 'block'
+        loadingAnimation.style.display = 'none'
+    } catch (trace) {
+        console.error(trace)
+        loadingAnimation.style.display = 'none'
+        errorAnimation.style.display = 'flex'
+        mainApp.style.display = 'none'
+    }
+}
+
+/**
+ * Make base layout visible
+ */
+function displayBaseLayout () {
+    document.querySelector('.container').style.display = 'block'
+}
+
+/**
+ * Get Main App Element Container
+ * @returns
+ */
+function getMainAppContainer () {
+    return document.querySelector('#mainApp')
+}
+
+/**
+ * Get Content Container DOM Element
+ */
+function getContentContainer () {
+    return document.querySelector('#content')
+}
+
+/**
+ * Get Loading Animation Element Container
+ * @returns
+ */
+function getLoadingAnimationContainer () {
+    return document.querySelector('.loadingAnimation')
+}
+
+/**
+ * Get Error Animation DOM Element Container
+ * @returns
+ */
+ function getErrorAnimationContainer () {
+    return document.querySelector('.errorAnimation')   
+}
+
+/**
+ * Get Navbar DOM element container
+ */
+function getNavbarContainer () {
+    return document.querySelector('#navbar')
+}
+
+/**
+ * Build element for surah item
+ * @param {object} quranData
+ * @param {string} quranData.name
+ * @param {string} quranData.translation
+ * @param {string} quranData.type
+ * @param {string|number} quranData.numberSurah
+ * @param {string|number} quranData.ayahCount
+ * @returns
+ */
+function SurahItem({ name, translation, type, numberSurah, ayahCount }) {
+    return `
+        <a class="surahItem" href="detail.html?numberSurah=${numberSurah}">
+            <div class="nameInfo">
+                <div class="name">${name}</div>
+                <div class="translation">${translation}</div>
+            </div>
+            <div class="additionalInfo">
+                <div>${type}</div>
+                <div>${numberSurah}:${ayahCount}</div>
+            </div>
+        </a>
+    `
+}
+
+/**
+ * Render surah item elements based from api data
+ * @param {*} quranData 
+ */
+function renderListSurahElements(quranData) {
+    const searchBar = getNavbarContainer().querySelector('.searchContainer input')
+
+    const surahListElements = quranData.reduce((elements, surah) => {
+        return elements += SurahItem({
+            name: surah.asma.id.long,
+            translation: surah.asma.translation.id,
+            type: surah.type.id,
+            numberSurah: surah.number,
+            ayahCount: surah.ayahCount
+        })
+    }, '')
+
+    getContentContainer().innerHTML = surahListElements
+    
+    searchBar.oninput = (event) => {
+        const filteredData = quranData.filter(surah => {
+            const surahName = surah.asma.id.long.toLowerCase().replace(/\'+/g, '')
+            const searchQuery = event.target.value.toLowerCase()
+            return surahName.includes(searchQuery)
+        })
+
+        getContentContainer().innerHTML = filteredData.reduce((elements, surah) => {
+            return elements += SurahItem({
+                name: surah.asma.id.long,
+                translation: surah.asma.translation.id,
+                type: surah.type.id,
+                numberSurah: surah.number,
+                ayahCount: surah.ayahCount
+            })
+        }, '')
+    }
+}
